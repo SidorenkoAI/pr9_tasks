6f172246c750ba127ee55aa8b94cefe3013dbb3d
Danil Belousov <danilbeloysov@ya.ru>
2024-01-24 16:26:08 +0700

    task2 Belousov

--- task2.py
+++ task2.py
@@ -2,9 +2,12 @@ class task2:
     def __init__(self):
         pass
     def greater(self, arg):
-        '''
-        :param arg: список целых чисел
-        :return: Если arg монотонно возрастает
-        (то есть каждый элемент больше предыдущего),
-        то вернуть True, иначе False
-        '''
+        a=arg[0]
+        arg.pop(0)
+        for i in arg:
+            if i<=a:
+                return False
+                break
+            a=i
+        return True
+
