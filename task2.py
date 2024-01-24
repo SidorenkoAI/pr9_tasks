6f5eff7da50348938580e83f1f0236a605bb579a
lavrentious <lavrent0@yandex.ru>
2024-01-24 18:28:41 +0700

    Kiryha_Task1

--- task2.py
+++ task2.py
@@ -1,10 +1,19 @@
 class task2:
+
     def __init__(self):
         pass
+
     def greater(self, arg):
-        '''
-        :param arg: список целых чисел
-        :return: Если arg монотонно возрастает
-        (то есть каждый элемент больше предыдущего),
-        то вернуть True, иначе False
-        '''
+        a = arg[0]
+        good = True
+        for i in arg:
+            print(i, a)
+            if i <= a and i != arg[0]:
+                good = False
+                break
+            else:
+                a = i
+        if good:
+            return True
+        else:
+            return False
--- task4.py
+++ task4.py
@@ -1,6 +1,9 @@
-def PalindromeFilter(arg, minLength):
-    '''
-    :param arg: список строк
-    :return: список строк, которые являются
-    палиндромами и имеют длину не меньше minLength
-    '''
+def PalindromeFilter(arg,minLength):
+
+    a = []
+
+    for i in arg:
+        if i==i[::-1] and len(i)>=minLength:
+            a.append(i)
+
+    return a
