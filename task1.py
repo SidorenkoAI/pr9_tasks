6acb4c55555d58bf757e6dc7e37e9bc5b67fb480
sai <sai@mail.ru>
2024-01-24 16:12:05 +0700

    task1 Сидоренко

--- task1.py
+++ task1.py
@@ -2,6 +2,7 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
+        return list(filter(lambda x: x % 2 ==0, arg))
         '''
         :param arg: список целых чисел
         :return: список четных чисел
