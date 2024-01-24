4f43177117dffa88c1f36147445fe0d33c7fc414
User5 <ryzkovartem2008@gmail.com>
2024-01-24 17:14:17 +0700

    task1-Рыжков Артём

--- task1.py
+++ task1.py
@@ -1,17 +1,11 @@
 class task1:
-    def __init__(self, numbers):
-        self.numbers = numbers
-
-    def giveEvenNumbers(self):
-        even_numbers = []
-        for number in self.numbers:
-            if number % 2 == 0:
-                even_numbers.append(number)
-        return even_numbers
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even_numbers = task1(numbers)
-result = even_numbers.giveEvenNumbers()
-print(result)
+    def __init__(self):
+        pass
+    def giveEvenNumbers(self, arg):
+        lst=[]
+        for i in arg:
+            if i%2==0:
+                lst.append(i)
+        return lst
 
 
