fb7ad8ef221492575d0c27a19427f93a3e9df5ae
Djerdux <92996251+Djerdux@users.noreply.github.com>
2024-01-24 16:35:46 +0700

    task1 Черданцев Илья

--- task4.py
+++ task4.py
@@ -1,6 +1,7 @@
-def PalindromeFilter(arg, minLength):
-    '''
-    :param arg: список строк
-    :return: список строк, которые являются
-    палиндромами и имеют длину не меньше minLength
-    '''
+class task4:
+    def PalindromeFilter(self,arg, minLength):
+        ans=[]
+        for i in arg:
+            if i==i[::-1] and len(i)>=minLength:
+                ans.append(i)
+        return ans
