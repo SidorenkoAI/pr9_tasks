714b949bb3a42efaab75af9d5b92a0ff179825a0
Djerdux <92996251+Djerdux@users.noreply.github.com>
2024-01-24 18:19:01 +0700

    task1 Черданцев Илья

--- User.py
+++ User.py
@@ -1,24 +1,13 @@
 import itertools
 class User:
-    '''
-    Модифицировать класс таким образом, чтобы для объектов
-    работал следующий код:
-    users = [User(), User(), User(), User(), User(), User()]
-    import random
-    for user in users:
-        user.addMoney(random.randint(0, 100000))
-    print(max(users))
-    '''
     userId = itertools.count()
     def __init__(self):
         self.id = next(User.userId)
         self.balance = 0
     def __str__(self):
-        return f'User ID {self.id}'
-
+        return f'User ID: {self.id}. Balance: {self.balance} rub.'
+    def __gt__(self, other):
+        return self.balance>other.balance
     def addMoney(self, nom, count):
-        '''
-        :param nom: номинал
-        :param count: количество купюр
-        метод должен увеличить баланс пользователя
-        '''
+        self.balance+=(nom*count)
+# class Admin:
