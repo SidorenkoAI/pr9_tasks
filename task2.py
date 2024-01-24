c3a0c2f93c4807beab9a085406f9ca75da505c9e
User5 <ryzkovartem2008@gmail.com>
2024-01-24 17:29:12 +0700

    task2-Рыжков Артём

--- task2.py
+++ task2.py
@@ -1,11 +1,9 @@
 class task2:
     def __init__(self):
         pass
-    k = False
-    def greater(self, arg):
-        for i in range(1, len(arg)):
-            if arg[i-1] < arg[i]:
-                k = True
-            else:
-                k = False
-        return k
+
+    def greater(self, arg: list):
+        arg2 = arg.copy()
+        arg2.sort()
+
+        return arg == arg2
