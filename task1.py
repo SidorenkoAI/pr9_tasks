4142cf4989b8abd5a49bff8a253c2a493add7042
Artemus22 <99726955+Artemus22@users.noreply.github.com>
2024-01-24 17:56:01 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,4 +2,4 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
-        return [num for num in arg if num % 2 == 0]
+        return [num1 for num1 in arg if num1 % 2 == 0]
