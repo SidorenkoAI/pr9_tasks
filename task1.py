96043b8c8eed136852f32dcaf2a41a9c307b1b84
lavrentious <lavrent0@yandex.ru>
2024-01-24 17:53:56 +0700

    Kiryha_Task1

--- task1.py
+++ task1.py
@@ -2,4 +2,4 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
-        return [num for num in arg if num % 2 == 0]
+        return [num for num in arg if num % 2 ==0]
