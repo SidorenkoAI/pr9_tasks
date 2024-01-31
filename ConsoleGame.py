1c452f95ce83ab91ae97a68021a7103db06a7c68
LORK228 <maksim041118@gmail.com>
2024-01-31 17:05:15 +0700

    rev2

--- ConsoleGame.py
+++ ConsoleGame.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools, random
 '''
 В некой игре-стратегии есть солдаты и герои. У всех есть свойство, содержащее
 уникальный номер объекта, и свойство, в котором хранится принадлежность команде. У
@@ -13,20 +13,36 @@ import itertools
 
 class Hero:
     HeroID = itertools.count()
-    def __init__(self, TeamNumber = "Red"):
-        self.id = Hero.HeroID
+    def __init__(self, TeamNumber = "0"):
+        self.id = next(Hero.HeroID)
         self.team = TeamNumber
         self.level = 0
+    def __str__(self):
+        return f"{self.id} {self.team}"
+    def __repr__(self):
+        return f"{self.id} {self.team}"
     def LevelChange(self):
         self.level += 1
 
 class Solder:
     SolderID = itertools.count()
 
-    def __init__(self, TeamNumber="Red"):
-        self.id = Solder.SolderID
+    def __init__(self, TeamNumber="0"):
+        self.id = next(Solder.SolderID)
         self.team = TeamNumber
-
+    def __str__(self):
+        return f"Солдат ID {self.id} из команды {self.team}"
+    def __repr__(self):
+        return self.__str__()
     def follow_to_hero(self, hero = Hero()):
         pass
 
+# a = [Hero('12'), Hero('13'), Hero('14')]
+# print(a)
+team = int(input())
+team_lst = {}
+while team:
+    hero = Hero(str(team))
+    team_lst[hero] = Solder(str(random.randint(2, 10)))
+    team -= 1
+print(list(team_lst.values()))
